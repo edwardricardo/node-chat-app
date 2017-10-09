51837bff5e713b50076c08c1e2fcaa5a2fdea6f1
Edward Velásquez Rodríguez <edward.velasquez.r@gmail.com>
2017-10-09 00:43:27 -0700

    Add messages form and showing incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -9,5 +9,17 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function(message){
-    console.log('New Message', message)
+    console.log('New Message', message);
+    const li = $('<li></li>');
+    li.text(`${message.from}: ${message.text}`)
+    $('#messages').append(li);
 })
+
+$('#message-form').on('submit', function(e){
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: $('[name=message]').val()
+    }, function(){})
+});
