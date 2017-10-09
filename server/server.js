9e1df02b33b390c3f259d44b80e236d13d3db2be
Edward Velásquez Rodríguez <edward.velasquez.r@gmail.com>
2017-10-08 18:31:05 -0700

    newMessage and createMessage events created

--- server/server.js
+++ server/server.js
@@ -14,6 +14,26 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    /*socket.emit('newEmail', {
+        from: 'myemil@example.com',
+        text: 'What is going on?',
+        createdAt: 123
+    });
+
+    socket.on('createEmail', (newEmail) => {
+        console.log('createEmail', newEmail);
+    })*/
+
+    socket.emit('newMessage', {
+        from: '@edward',
+        text: 'What is going on?',
+        createdAt: 123
+    })
+
+    socket.on('createMessage', (newMessage) => {
+        console.log('createMessage', newMessage)
+    })
+
     socket.on('disconnect', (socket) => {
         console.log('User disconnected');
     });
