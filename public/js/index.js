ac8408bb3f8eda93a4882ecd022eeeabe0cd0321
Edward Velásquez Rodríguez <edward.velasquez.r@gmail.com>
2017-10-09 22:32:30 -0700

    Add Mustache js for messages templates

--- public/js/index.js
+++ public/js/index.js
@@ -8,24 +8,29 @@ socket.on('disconnect', function () {
     console.log('Disconnected from server')
 });
 
-socket.on('newMessage', function(message){
-    const formattedTime = moment(message.createdAt).format('h:mm a');
-    const li = $('<li></li>');
-    li.text(`${message.from} - ${formattedTime}: ${message.text}`)
-    $('#messages').append(li);
-})
+socket.on('newMessage', function (message) {
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
+
+    jQuery('#messages').append(html);
+});
 
 socket.on('newLocationMessage', function (message) {
-    const formattedTime = moment(message.createdAt).format('h:mm a');
-    
-    const li = $('<li></li>');
-    const a = $('<a target="_blank">My current location</a>')
-    li.text(`${message.from} - ${formattedTime}: `);
-    a.attr('href', message.url);
-    li.append(a);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery('#location-message-template').html();
+    var html = Mustache.render(template, {
+        from: message.from,
+        url: message.url,
+        createdAt: formattedTime
+    });
 
-    $('#messages').append(li);
-})
+    jQuery('#messages').append(html);
+});
 
 $('#message-form').on('submit', function(e){
     e.preventDefault();
